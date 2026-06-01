0c6d3fc98f342dc1c8f15956f385a3d01827fb2c
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-06-01 23:50:26 +0530

    Migrated the schema

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hey there");
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const client = new PrismaClient({
+  adapter,
+});
+
+async function create(){
+    await client.user.create({
+        data: {username: "Vinay", password: "hhuh"}
+    })
+}
+create()
