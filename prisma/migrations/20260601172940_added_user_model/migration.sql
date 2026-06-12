8a13e4b7fa94af2ec4d1a4856792a2ff028450dc
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-06-12 18:16:49 +0530

    Initialised the project again

--- prisma/migrations/20260601172940_added_user_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "is_done" BOOLEAN NOT NULL,
-    "user_id" INTEGER NOT NULL,
-
-    CONSTRAINT "Todo_pkey" PRIMARY KEY ("title")
-);
