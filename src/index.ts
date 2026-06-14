508a212b616b2dccf201f354499af3ed6601aef4
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-06-14 23:07:17 +0530

    Created the repo again

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "dotenv/config"
+import {PrismaPg} from "@prisma/adapter-pg"
+import { PrismaClient } from "./generated/prisma/client.js"
+
+const connectionString = process.env.DATABASE_URL
+
+const adapter = new PrismaPg({connectionString})
+const prisma = new PrismaClient({adapter})
+
+async function main(){
+    await prisma.user.create({
+        data: {
+            username: "vhgasvuj"
+        }
+    })
+}
+
+main()
